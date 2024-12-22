2a71978c126ec64d40b8a9ed956bd4758a468528
Daniel Stirnimann <daniel.stirnimann@switch.ch>
2024-12-22 10:31:43 +0100

    make pattern domain agnostic

--- sync-theme.sh
+++ sync-theme.sh
@@ -8,4 +8,4 @@
 # --delete: This option deletes files that are not in the source directory from the
 #     destination directory. This ensures that the destination only contains files
 #     that match the source.
-rsync -a --delete gp-winterthur/ winterthur-marathon.ch:www/test.gp-winterthur.ch/wordpress/wp-content/themes/gp-winterthur/
+rsync -a --delete gp-winterthur/ winterthur-marathon.ch:www/stage.gp-winterthur.ch/wordpress/wp-content/themes/gp-winterthur/
