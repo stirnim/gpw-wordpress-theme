d144dab5c5fb8d9b474eb0cf9f4e0ff44d72b220
Daniel Stirnimann <daniel.stirnimann@switch.ch>
2024-12-28 12:45:57 +0100

    fix file permission before sync

--- sync-theme.sh
+++ sync-theme.sh
@@ -8,4 +8,5 @@
 # --delete: This option deletes files that are not in the source directory from the
 #     destination directory. This ensures that the destination only contains files
 #     that match the source.
+chmod -R o+r gp-winterthur/
 rsync -a --delete gp-winterthur/ winterthur-marathon.ch:www/stage.gp-winterthur.ch/wordpress/wp-content/themes/gp-winterthur/
